e28f847d26a01d177693680a3a6fb7e0886a33e3
Есенжанұлы Бекжан Малбагаров <malbagarov_b@cpi.nis.edu.kz>
2022-08-19 17:16:38 +0600

    Initial commit

---
+++ ecommerce/store/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import *
+
+def store(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/Store.html',context)
+
+def cart(request):
+
+    if request.user:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0,'get_cart_items':0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/Cart.html', context)
+
+def checkout(request):
+    if request.user:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0,'get_cart_items':0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/Checkout.html', context)
+
